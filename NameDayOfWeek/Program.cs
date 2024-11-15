a96ee5e71f75a032148d41f9d721126f37bf460d
Vadym Chernyshov <vadym1927dk@gmail.com>
2024-11-15 10:17:58 +0200

    Add project files.

---
+++ NameDayOfWeek/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NameDayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
