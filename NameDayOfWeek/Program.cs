67e2dbdc68bd2c56f54a79c6abe608f31040520d
Vadym Chernyshov <vadym1927dk@gmail.com>
2024-11-15 10:41:35 +0200

    Create all

--- NameDayOfWeek/Program.cs
+++ NameDayOfWeek/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter the number of the day of the week");
+            string customersInputValue = Console.ReadLine(); //ввід користувача
+
+            //switch
+            string dayOfWeek = customersInputValue switch
+            {
+                "1" => "Monday",
+                "2" => "Tuesday",
+                "3" => "Wendnesday",
+                "4" => "Thuersday",
+                "5" => "Friday",
+                "6" => "Saturday",
+                "7" => "Sunday",
+                  _ => "Incorrect input. Please try again"
+            };
+
+            Console.WriteLine(dayOfWeek);
         }
     }
 }
